8355ef6569f2f601854bfda6c1335c1ffc829c3f
Bpparajuli <bishesworparajuli@gmail.com>
2026-07-01 16:44:46 +0545

    update fileinfo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,18 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use League\Flysystem\Filesystem as Flysystem;
+use League\Flysystem\Local\LocalFilesystemAdapter as FlysystemLocalAdapter;
+use League\Flysystem\PathPrefixing\PathPrefixedAdapter;
+use League\Flysystem\ReadOnly\ReadOnlyFilesystemAdapter;
+use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
+use League\Flysystem\Visibility;
+use League\MimeTypeDetection\ExtensionMimeTypeDetector;
+use Illuminate\Filesystem\LocalFilesystemAdapter as LaravelLocalAdapter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,5 +24,47 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        Storage::extend('local', function ($app, $config) {
+            $visibility = PortableVisibilityConverter::fromArray(
+                $config['permissions'] ?? [],
+                $config['directory_visibility'] ?? $config['visibility'] ?? Visibility::PRIVATE,
+            );
+
+            $links = ($config['links'] ?? null) === 'skip'
+                ? FlysystemLocalAdapter::SKIP_LINKS
+                : FlysystemLocalAdapter::DISALLOW_LINKS;
+
+            $adapter = new FlysystemLocalAdapter(
+                $config['root'],
+                $visibility,
+                $config['lock'] ?? LOCK_EX,
+                $links,
+                new ExtensionMimeTypeDetector,
+            );
+
+            if (($config['read-only'] ?? false) === true) {
+                $adapter = new ReadOnlyFilesystemAdapter($adapter);
+            }
+
+            if (! empty($config['prefix'])) {
+                $adapter = new PathPrefixedAdapter($adapter, $config['prefix']);
+            }
+
+            if (str_contains($config['endpoint'] ?? '', 'r2.cloudflarestorage.com')) {
+                $config['retain_visibility'] = false;
+            }
+
+            $flysystem = new Flysystem($adapter, Arr::only($config, [
+                'directory_visibility',
+                'disable_asserts',
+                'retain_visibility',
+                'temporary_url',
+                'url',
+                'visibility',
+            ]));
+
+            return (new LaravelLocalAdapter($flysystem, $adapter, $config));
+        });
     }
 }
